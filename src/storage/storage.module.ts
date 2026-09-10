8cb7d402a158a1879630fa47717d882db3b83b89
🐧Linux of Power <linuxofpower@gmail.com>
2026-09-10 21:38:59 +0200

    feat: add shared storage module

---
+++ src/storage/storage.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { JobStoreService } from './job-store.service';
+import { JobWorkerService } from './job-worker.service';
+import { RcloneService } from './rclone.service';
+import { SharedRootsService } from './shared-roots.service';
+import { StorageController } from './storage.controller';
+import { StorageService } from './storage.service';
+
+@Module({
+  controllers: [StorageController],
+  providers: [RcloneService, SharedRootsService, StorageService, JobStoreService, JobWorkerService],
+  exports: [RcloneService, JobStoreService],
+})
+export class StorageModule {}
