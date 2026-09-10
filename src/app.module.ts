0778ade4a539913f7a1f61a717a2e8d6c294e966
🐧Linux of Power <linuxofpower@gmail.com>
2026-09-10 21:37:14 +0200

    feat: wire Nest application module

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ApiKeyGuard } from './common/api-key.guard';
+import { HealthController } from './health.controller';
+import { StorageModule } from './storage/storage.module';
+
+@Module({
+  imports: [StorageModule],
+  controllers: [HealthController],
+  providers: [{ provide: APP_GUARD, useClass: ApiKeyGuard }],
+})
+export class AppModule {}
